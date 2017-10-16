d0cea664ee03bb3cf11644e0de0e9fb045631153
Preston Donovan <prestonmdonovan@gmail.com>
2017-10-16 13:21:32 -0400

    Added some documentation.

--- GraphHomogParams_lattice.m
+++ GraphHomogParams_lattice.m
@@ -1,5 +1,36 @@
 classdef GraphHomogParams_lattice
-
+    % An auxiliary class for creating a GraphHomogInput object when the
+    % following conditions are satisfied:
+    %   -Dimension is 2 or 3
+    %   -Obstruction is a square or a circle
+    %   -Graph invariant under integer vector scaling (periodicity 1)
+    % Inputs:
+    %   1) dim (scalar) = dimension (2 or 3)
+    %   2) geometry (string) = options:
+    %       "circle", 
+    %       "circleSlowdown", 
+    %       "square", 
+    %       "squareBonding", 
+    %       "squareBdyRepel", 
+    %       "squareBdyAttract", 
+    %       "squareBdySlow"
+    %   3) D0 (scalar) = free diffusivity (typically 1)
+    %   4) rho (scalar) = 2*(a+R)/L where a = particle radius, R = obstruction
+    %   radius, and L = obstruction spacing
+    %   5) m (scalar) = 1/(mesh spacing) = 1/(path length)
+    %   6) rate = currently obsolete field
+    %   7) diagJumps (scalar) = 0 if no diagonal jumps, 1 if diagonal jumps
+    %   8) ctr = center of obstruction (center of unit cell is (.5,...,.5)
+    %
+    % Other fields:
+    %   1) rateCoeffs (object) = scalar fields containing certain
+    %   rate function parameters:
+    %       .dist = specifies distance for the squareBonding,
+    %       squareBdyRepel, squareBdyAttract, and squareBdySlow geometries
+    %       .alpha = multiplier on drift term for rate (0 = no drift, 1 =
+    %       repulsion, -1 = attraction
+    %       .K1 = K1 coefficient for drift
+    %       .K2 = K2 coefficient for drift
     properties
         
         dim
