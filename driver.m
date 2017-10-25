ef09b2d18d068041d115913181e12f3bd04d8454
Preston Donovan <prestonmdonovan@gmail.com>
2017-10-25 16:27:53 -0400

    Created public driver.

--- driver.m
+++ driver.m
@@ -1,15 +1,14 @@
-
-saveOn = 0;
+%% A simple driver
 plotOn = 1;
 
 % geometry parameters
 dim = 2;
-geometryName = 'square';
+geometryName = 'circle';
 D0 = 1;
-rhoVals = [.25 .5 .75];
-mVals = 2.^[3 4];
-diagJumps = 2; % diagJumps = 2 => corrected diagonal jumps
-ctr = .5;
+rho = .5;
+m = 5;
+diagJumps = 0; % diagJumps = 2 => corrected diagonal jumps
+obCtr = .5;
 specialSetting_m2 = 'none';
 
 % monte carlo parameters
@@ -28,39 +27,17 @@ rateCoeffs.K1 = 25;
 rateCoeffs.K2 = 10;
 rateCoeffs.delta = delta;
 
-clear results;
-for i = 1:length(mVals)
-    m = mVals(i);
-    for j = 1:length(rhoVals)
-        rho = rhoVals(j);
+geometry.dim = dim;
+geometry.name = geometryName;
+geometry.m = m;
+geometry.rho = rho;
+geometry.diagJumps = diagJumps;
+geometry.ctr = obCtr;
+geometry.specialSetting_m2 = specialSetting_m2;
+geometry.rateCoeffs = rateCoeffs;
         
-        geometry.dim = dim;
-        geometry.name = geometryName;
-        geometry.m = m;
-        geometry.rho = rho;
-        geometry.diagJumps = diagJumps;
-        geometry.ctr = ctr;
-        geometry.specialSetting_m2 = specialSetting_m2;
-        geometry.rateCoeffs = rateCoeffs;
-        
-        [L,nodes,edges,edgeRates,edgeJumps] = homogInputs_lattice(dim,geometryName,D0,rho,m,rate,rateCoeffs,diagJumps,ctr,specialSetting_m2);
+[L,nodes,edges,edgeRates,edgeJumps] = homogInputs_lattice(...
+    dim,geometryName,D0,rho,m);
 
-        curRes = effDiff(L,nodes,edges,edgeRates,edgeJumps,numTraj,startNodeInd,geometry);
-        
-        results(i,j) = curRes;
-        
-    end
-    
-end
-
-if plotOn
-    for i = 1:length(mVals)
-    
-        plotRhoVsEffDiff(results(i,:));
-        
-    end
-end
-
-if saveOn
-    saveResults(results);
-end
+results = effDiff(L,nodes,edges,edgeRates,edgeJumps,...
+                    numTraj,startNodeInd,geometry);
