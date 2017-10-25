9126098b06f34d943036a25077bdd67f0c80ec88
Preston Donovan <prestonmdonovan@gmail.com>
2017-10-25 12:54:32 -0400

    Fixed inputs variables.

--- checkDetailedBalance.m
+++ checkDetailedBalance.m
@@ -1,25 +1,23 @@
-function [ cond1, cond2 ] = checkDetailedBalance( ghInput, results_homog )
+function [ cond1, cond2, cond3 ] = checkDetailedBalance( nodes,edges,edgeRates,L, pi0 )
 %CHECKDETAILEDBALANCE Summary of this function goes here
 %   Detailed explanation goes here
 
 %% check detailed balance
-nNodes  = size(ghInput.nodes,1);
-nEdges = size(ghInput.edges,1);
+nNodes  = size(nodes,1);
+nEdges = size(edges,1);
 g = rand(nNodes,1);
 f = rand(nNodes,1);
 
-rates = ghInput.edgeRates;
-L = ghInput.L;
-pi0 = results_homog.pi0;
-pi0Start = pi0(ghInput.edges(:,1));
-pi0End = pi0(ghInput.edges(:,2));
+rates = edgeRates;
 
 cond1 = 0;
+cond2 = zeros(nEdges,1);
+
 ratesPrime = zeros(nEdges,1);
 for i = 1:nEdges
     % check L = L^T wrt pi
-    edgeStart = ghInput.edges(i,1);
-    edgeEnd = ghInput.edges(i,2);
+    edgeStart = edges(i,1);
+    edgeEnd = edges(i,2);
     cond1 = cond1 + (g(edgeEnd)*f(edgeStart) - f(edgeEnd)*g(edgeStart))*rates(i)*pi0(edgeStart);
     
     % check other thing
@@ -28,10 +26,7 @@ for i = 1:nEdges
         (g(edgeStart)*f(edgeEnd) - f(edgeStart)*g(edgeEnd))*ratesPrime(i)*pi0(edgeEnd);
     
 end
-cond3 = results_homog.pi0(ghInput.edges(:,1)).*ghInput.edgeRates - results_homog.pi0(ghInput.edges(:,2)).*ratesPrime;
-
-fprintf('\n');
-fprintf('Unit cell solvability: %.10f.\n',sum(abs(results_homog.unitCell_solvability)));
+cond3 = pi0(edges(:,1)).*edgeRates - pi0(edges(:,2)).*ratesPrime;
 
 if sum(abs(cond1)) < 1e-10
     fprintf('L = L^T wrt pi is satisfied (%.10f).\n',sum(abs(cond1)));
