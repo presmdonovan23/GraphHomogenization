571abd358d66e7165da52c933fe4692448927540
Preston Donovan <prestonmdonovan@gmail.com>
2017-10-25 14:33:17 -0400

    Removed usage of outdated function latticeSetup.

--- driver_2x2.m
+++ driver_2x2.m
@@ -1,13 +1,13 @@
 numTraj = 0;
 specialSetting_m2 = 'blockOneSite';
 specialSetting_m2 = 'slowOneSite';
-delta = .1; % only required if setting = slowOneSite
+delta = .001; % only required if setting = slowOneSite
 
 % DO NOT CHANGE
 rho = 0;
 m = 2;
 dim = 2;
-geometry = 'square';
+geometryName = 'square';
 diagJumps = 0;
 D0 = 1;
 rate = [];
@@ -17,11 +17,11 @@ rateCoeffs.K2 = 10;
 rateCoeffs.delta = delta;
 ctr = [];
 negateSteps = 1;
-
+startNodeInd = [];
+plotOn = 0;
 %%
-params = LatticeParams(dim,geometry,D0,rho,m,rate,rateCoeffs,diagJumps,ctr);
-
-[L,nodes,edges,edgeRates,edgeJumps] = latticeSetup(params);
+[L,nodes,edges,edgeRates,edgeJumps] = homogInputs_lattice(...
+    dim,geometryName,D0,rho,m,rate,rateCoeffs,diagJumps,ctr,specialSetting_m2);
 
 LUtime = [];
 
@@ -52,30 +52,27 @@ elseif strcmpi(specialSetting_m2,'slowOneSite')
     unitCell_time = [];
 end
 
-[Deff,DeffTerm1,DeffTerm2] = buildEffDiff( edges, edgeRates, edgeJumps, pi0, unitCell_soln );
+[Deff, DeffTerm1, DeffTerm2] = buildEffDiff( edges, edgeRates, edgeJumps, pi0, unitCell_soln );
 
-MC = getDeff_MC( L, nodes, edges, edgeJumps, numTraj, startNodeInd, plotOn, negateSteps );
-
-results.params = params;
-results.L = L;
-results.nodes = nodes;
-results.edges = edges;
-results.edgeRates = edgeRates;
-results.edgeJumps = edgeJumps;
-results.LU_time = LUtime;
-results.pi0 = pi0;
-results.pi0_res = pi0_relRes;
-results.pi0_flag = pi0_flag;
-results.pi0_time = pi0_time;
-results.unitCell_solvability = unitCell_solvability;
-results.unitCell_RHS = unitCell_RHS;
-results.unitCell_soln = unitCell_soln;
-results.unitCell_relRes = unitCell_relRes;
-results.unitCell_flag = unitCell_flag;
-results.unitCell_time = unitCell_time;
-results.Deff_mat = Deff;
-results.Deff_term1 = DeffTerm1;
-results.Deff_term2 = DeffTerm2;
-results.Deff = Deff(1);
-
-results.monteCarlo = MC;
+results.L =                     L;
+results.nodes =                 nodes;
+results.edges =                 edges;
+results.edgeRates =             edgeRates;
+results.edgeJumps =             edgeJumps;
+results.LU_time =               LUtime;
+results.pi0 =                   pi0;
+results.pi0_res =               pi0_relRes;
+results.pi0_flag =              pi0_flag;
+results.pi0_time =              pi0_time;
+results.unitCell_solvability =  unitCell_solvability;
+results.unitCell_RHS =          unitCell_RHS;
+results.unitCell_soln =         unitCell_soln;
+results.unitCell_relRes =       unitCell_relRes;
+results.unitCell_flag =         unitCell_flag;
+results.unitCell_time =         unitCell_time;
+results.Deff_mat =              Deff;
+results.Deff_term1 =            DeffTerm1;
+results.Deff_term2 =            DeffTerm2;
+results.Deff =                  Deff(1);
+results.geometry.name =         geometryName;
+results.mc = effDiff_mc(L, nodes, edges, edgeJumps, numTraj, startNodeInd, plotOn, negateSteps );
