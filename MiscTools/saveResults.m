16e6667a9720b8c15d19e41a51c21f120ca308d6
Preston Donovan <prestonmdonovan@gmail.com>
2017-11-02 13:29:27 -0400

    Now saves results in Results/ dir.

--- MiscTools/saveResults.m
+++ MiscTools/saveResults.m
@@ -1,14 +1,34 @@
-function fullFilename = saveResults(results)
+function fullFilename = saveResults(results,dirname)
 
-dirname = sprintf('Results_%dd_%s',results(1).geometry.dim,results(1).geometry.name);
-
-if results(1).geometry.diagJumps == 1
-    dirname = [dirname '_diagJumps'];
-elseif results(1).geometry.diagJumps == 2
-    dirname = [dirname '_diagJumpsCorrected'];
+if nargin < 2 || isempty(dirname)
+    dirname = 'Results';
 end
 
-filename = sprintf('results_%s',myClock(6));
+geometry = results(1).geometry;
+
+dimStr = sprintf('%dd',geometry.dim);
+geometryStr = geometry.name;
+
+specialSettingStr = geometry.specialSetting;
+
+if strcmpi(specialSettingStr,'none')
+    specialSettingStr = [];
+end
+
+diagJumpStr = [];
+if geometry.diagJumps == 1
+    diagJumpStr = 'diagJumps';
+elseif geometry.diagJumps == 2
+    diagJumpStr = 'diagJumpsCorrected';
+end
+
+c = clock;
+dateStr = sprintf('%.2d_%.2d_%.2d_%.2d_%.2d_%.2d',c(1),c(2),c(3),c(4),c(5),round(c(6)));
+
+filename = [dimStr '_' geometryStr '_' specialSettingStr '_' diagJumpStr '_' dateStr];
+filename = strrep(filename,'__','_');
+filename = strrep(filename,'__','_');
+filename = strrep(filename,'__','_');
 
 fullFilename = [dirname '/' filename];
 
